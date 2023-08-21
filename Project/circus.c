bd7242fac513573932fac612dcf9ab3face663f2
Mateus Dantas <mateus.dantass460@gmail.com>
2023-08-21 13:07:34 -0300

    Create circus.c

---
+++ Project/circus.c
@@ -0,0 +1,11 @@
+// Universidade Federal do Rio Grande do Norte
+// Disciplina DCT1106 -- Programação
+// Projeto Sistema de Gestão de um Circo
+// Developed by Mateus Dantas de Oliveira
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+}
