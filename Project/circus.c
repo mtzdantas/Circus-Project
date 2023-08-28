61a9118995eadb410d71eff750445116d274f364
Mateus Dantas <mateus.dantass460@gmail.com>
2023-08-28 14:32:57 -0300

    Update circus.c

--- Project/circus.c
+++ Project/circus.c
@@ -1,10 +1,11 @@
 // Universidade Federal do Rio Grande do Norte
-// Disciplina DCT1106 -- Programa√ß√£o
-// Projeto Sistema de Gest√£o de um Circo
+// Disciplina DCT1106 -- ProgramaÁ„o
+// Projeto Sistema de Gest„o de um Circo
 // Developed by Mateus Dantas de Oliveira
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 void intro_ingressos(void);
 
@@ -19,16 +20,18 @@ char option;
 int stop = 1;
 
 int main(){
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
     while (stop == 1){
         system("clear||cls");
-        printf("[1]Atra√ß√µes\n"); printf("[2]Ingressos\n"); printf("[3]Sobre n√≥s\n");
-        printf("[4]Desenvolvedor\n"); printf("[5]Administra√ß√£o\n"); printf("[6]Sair\n");
-        printf("DIGITE A OP√á√ÉO DESEJADA: ");
+        printf("[1]AtraÁıes\n"); printf("[2]Ingressos\n"); printf("[3]Sobre nÛs\n");
+        printf("[4]Desenvolvedor\n"); printf("[5]AdministraÁ„o\n"); printf("[6]Sair\n");
+        printf("DIGITE A OP«√O DESEJADA: ");
         scanf("%s", &option);
         switch (option){
             case '1':
                 system("clear||cls");
-                printf("ATRA√á√ïES CADASTRADAS.\n");
+                printf("ATRA«’ES CADASTRADAS.\n");
                 system("pause");
                 break;
             case '2':
@@ -50,8 +53,10 @@ int main(){
                 stop = 0;
                 break;
             default:
-                printf("POR FAVOR, SELECIONE UMA OP√á√ÉO V√ÅLIDA.\n");
+                system("clear||cls");
+                printf("POR FAVOR, SELECIONE UMA OP«√O V¡LIDA.\n");
                 system("pause");
+                system("clear||cls");
                 break;
         }
     }
@@ -63,32 +68,32 @@ void intro_ingressos(void){
         system("clear||cls");
         printf("[1]Comprar\n"); printf("[2]Cancelar\n"); printf("[3]Meu Carrinho\n");
         printf("[4]Finalizar\n"); printf("[5]Compras Realizadas\n"); printf("[6]Voltar\n");
-        printf("DIGITE A OP√á√ÉO DESEJADA: ");
+        printf("DIGITE A OP«√O DESEJADA: ");
         scanf("%s", &option);
         switch (option){
             case '1':
                 system("clear||cls");
-                printf("COMPRAR\n"); // Aqui ter√° que informar o c√≥digo do show que ser√° consultado ao clicar enter.
+                printf("COMPRAR\n"); // Aqui ter· que informar o cÛdigo do show que ser· consultado ao clicar enter.
                 system("pause");
                 break;
             case '2':
                 system("clear||cls");
-                printf("CANCELAR COMPRAS\n"); // Aqui ter√° que informar o c√≥digo do show que ser√° consultado ao clicar enter.
+                printf("CANCELAR COMPRAS\n"); // Aqui ter· que informar o cÛdigo do show que ser· consultado ao clicar enter.
                 system("pause");
                 break;
             case '3':
                 system("clear||cls");
-                printf("MEU CARRINHO\n"); // Aqui listar√° o que o cliente comprou at√© agora.
+                printf("MEU CARRINHO\n"); // Aqui listar· o que o cliente comprou atÈ agora.
                 system("pause");
                 break;
             case '4':
                 system("clear||cls");
-                printf("FINALIZAR COMPRA\n"); // Aqui vai gerar um c√≥digo para pagar e ao ser pago a compra √© aprovada,
-                system("pause"); // diminuindo a quantidade de vagas e cadastrando nas vendas daquele evento. Pedir n√∫mero/CPF/nome.
+                printf("FINALIZAR COMPRA\n"); // Aqui vai gerar um cÛdigo para pagar e ao ser pago a compra È aprovada,
+                system("pause"); // diminuindo a quantidade de vagas e cadastrando nas vendas daquele evento. Pedir n˙mero/CPF/nome.
                 break;
             case '5':
                 system("clear||cls");
-                printf("COMPRAS REALIZADAS\n"); // Aqui mostrar√° ingressos comprados quando for informado nome completo/CPF.
+                printf("COMPRAS REALIZADAS\n"); // Aqui mostrar· ingressos comprados quando for informado nome completo/CPF.
                 system("pause");
                 break;
             case '6':
@@ -96,7 +101,7 @@ void intro_ingressos(void){
                 stop = 1;
                 break; 
             default:
-                printf("POR FAVOR, SELECIONE UMA OP√á√ÉO V√ÅLIDA.\n");
+                printf("POR FAVOR, SELECIONE UMA OP«√O V¡LIDA.\n");
                 system("pause");
                 break;
         }
@@ -107,10 +112,10 @@ void intro_profissional(void){
     stop = 0;
     while (stop == 0){
         system("clear||cls");
-        printf("[1]Eventos\n"); // Usar no cadastro a localiza√ß√£o, hor√°rio, data, pre√ßo
-        printf("[2]Relat√≥rios\n"); // vagas disponibilizadas e c√≥digo de show (para acessar informa√ß√µes facilmente).
+        printf("[1]Eventos\n"); // Usar no cadastro a localizaÁ„o, hor·rio, data, preÁo
+        printf("[2]RelatÛrios\n"); // vagas disponibilizadas e cÛdigo de show (para acessar informaÁıes facilmente).
         printf("[3]Voltar\n");
-        printf("DIGITE A OP√á√ÉO DESEJADA: ");
+        printf("DIGITE A OP«√O DESEJADA: ");
         scanf("%s", &option);
         switch (option){
             case '1':
@@ -123,7 +128,7 @@ void intro_profissional(void){
                 stop = 1;
                 break;
             default:
-                printf("POR FAVOR, SELECIONE UMA OP√á√ÉO V√ÅLIDA.\n");
+                printf("POR FAVOR, SELECIONE UMA OP«√O V¡LIDA.\n");
                 system("pause");
                 break;
         }
@@ -136,7 +141,7 @@ void profissional_eventos(void){
         system("clear||cls");
         printf("[1]Listar\n"); printf("[2]Cadastrar\n"); printf("[3]Cancelar\n");
         printf("[4]Alterar\n"); printf("[5]Voltar\n");
-        printf("DIGITE A OP√á√ÉO DESEJADA: ");
+        printf("DIGITE A OP«√O DESEJADA: ");
         scanf("%s", &option);
         switch (option){
             case '1':
@@ -148,7 +153,7 @@ void profissional_eventos(void){
                 system("clear||cls");
                 printf("CADASTRAR EVENTOS\n");
                 system("pause");
-                // Usar no cadastro a localiza√ß√£o, hor√°rio, data, pre√ßo, vagas disponibilizadas e c√≥digo de show.
+                // Usar no cadastro a localizaÁ„o, hor·rio, data, preÁo, vagas disponibilizadas e cÛdigo de show.
                 break;
             case '3':
                 system("clear||cls");
@@ -164,7 +169,7 @@ void profissional_eventos(void){
                 stop = 0;
                 break;
             default:
-                printf("POR FAVOR, SELECIONE UMA OP√á√ÉO V√ÅLIDA.\n");
+                printf("POR FAVOR, SELECIONE UMA OP«√O V¡LIDA.\n");
                 system("pause");
                 break;
         }
@@ -173,27 +178,27 @@ void profissional_eventos(void){
 
 void profissional_relatorios(void){
     system("clear||cls");
-    printf("GERANDO RELAT√ìRIOS...\n"); // Informa√ß√µes de quantas vendas foram feitas, e quanto foi arrecatado.
+    printf("GERANDO RELAT”RIOS...\n"); // InformaÁıes de quantas vendas foram feitas, e quanto foi arrecatado.
     system("pause");
 }
 
 void desenvolvedor(void){
     system("clear||cls");
     printf("~ DESENVOLVEDOR \n"); printf("===================\n\n");
-    printf("O projeto foi desenvolvido com o intuito de melhorar a gest√£o\n");
+    printf("O projeto foi desenvolvido com o intuito de melhorar a gest„o\n");
     printf("de eventos e ingressos de um circo, trazendo praticidade para\n");
-    printf("clientes e funcion√°rios.\n");
+    printf("clientes e funcion·rios.\n");
     printf("~ Developed By Mateus Dantas de Oliveira.\n");
     system("pause");
 }
 
 void sobre(void){
     system("clear||cls");
-    printf("~ SOBRE N√ìS \n"); printf("===============\n\n");
-    printf("Bem-vindos ao nosso m√°gico mundo de divers√£o e maravilhas!\n");
+    printf("~ SOBRE N”S \n"); printf("===============\n\n");
+    printf("Bem-vindos ao nosso m·gico mundo de divers„o e maravilhas!\n");
     printf("Aqui, no nosso amado circo, estamos prontos para transportar\n");
-    printf("voc√™ para um universo de encanto, risos e espet√°culos incr√≠veis.\n");
+    printf("vocÍ para um universo de encanto, risos e espet·culos incrÌveis.\n");
     printf("Permita-nos apresentar o 'GRAN C-IRCO' - um lugar onde os sonhos\n");
-    printf("ganham vida sob a grande lona e onde a imagina√ß√£o n√£o tem limites.\n");
+    printf("ganham vida sob a grande lona e onde a imaginaÁ„o n„o tem limites.\n");
     system("pause");
 }
