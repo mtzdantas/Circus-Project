113826a0c02d5bde926b01d2e304590a103a6b84
Mateus Dantas <mateus.dantass460@gmail.com>
2023-10-31 15:52:20 -0300

    Update profissional.c

--- profissional.c
+++ profissional.c
@@ -3,23 +3,22 @@
 #include <string.h>
 #include "profissional.h"
 #include "prof_eventos.h"
-#include "util.h"
 
 int verificar_profissional(){
-    char entrar[2][9] = {"circo", "circo123"};
-    char tryentrar[2][9];
+    char entlogin[] = "circo", entsenha[] = "circo123";
+    char trylogin[6], trysenha[9];
     int checklog = 1, checksen = 1, cancel = 1;
     while (checklog != 0 || checksen != 0){
-        limpaTela();
+        system("clear||cls");
         printf("===============================\n        ~ GRAN C-IRCO ~\n       LOGIN EMPRESARIAL\n===============================\n");
         printf("INFORME O LOGIN:\n");
-        gets(tryentrar[0]);
+        fgets(trylogin, 6, stdin);
         fflush(stdin);
         printf("INFORME A SENHA:\n");
-        gets(tryentrar[1]);
+        fgets(trysenha, 9, stdin);
         fflush(stdin);
-        checklog = strcmp(entrar[0], tryentrar[0]);
-        checksen = strcmp(entrar[1], tryentrar[1]);
+        checklog = strcmp(entlogin, trylogin);
+        checksen = strcmp(entsenha, trysenha);
         if (checklog == 0 && checksen == 0) {
             printf("LOGIN REALIZADO COM SUCESSO.\n");
             printf("\n- PRESSIONE ENTER PARA CONTINUAR.");
@@ -44,7 +43,7 @@ void intro_profissional(void){
     int option;
     int stop = 0;
     while (stop == 0){
-        limpaTela();
+        system("clear||cls");
         printf("===============================\n        ~ GRAN C-IRCO ~\n     BEM VINDO FUNCIONARIO\n===============================\n");
         printf("[1]Eventos\n"); 
         printf("[2]Relatorios\n");
@@ -74,7 +73,7 @@ void profissional_eventos(void){
     int option;
     int stop = 1;
     while (stop == 1){
-        limpaTela();
+        system("clear||cls");
         printf("===============================\n        ~ GRAN C-IRCO ~\n            EVENTOS\n===============================\n");
         printf("[1]Listar\n"); printf("[2]Cadastrar\n"); printf("[3]Cancelar\n");
         printf("[4]Alterar\n"); printf("[5]Voltar\n");
@@ -106,7 +105,7 @@ void profissional_eventos(void){
 }
 
 void profissional_relatorios(void){
-    limpaTela();
+    system("clear||cls");
     printf("===============================\n        ~ GRAN C-IRCO ~\n           RELATORIO\n===============================\n");
     printf("GERANDO RELATORIOS...\n"); // Informacões de quantas vendas foram feitas, e quanto foi arrecatado.
     printf("\n- PRESSIONE ENTER PARA CONTINUAR.");
